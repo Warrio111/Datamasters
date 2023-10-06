1b3ebde141245451f8b0e4d9e17c036eda2346ae
Robert Benavides <robertbenavidesgomez@gmail.com>
2023-10-07 00:16:07 +0200

    Test for instanceOf OnlineStoreTests

--- src/test/java/com/datamasters/vista/OnlineStoreTests.java
+++ src/test/java/com/datamasters/vista/OnlineStoreTests.java
@@ -1,13 +1,20 @@
 package com.datamasters.vista;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
 
-@SpringBootTest
-class OnlineStoreTests {
 
-	@Test
-	void contextLoads() {
+public class OnlineStoreTests {
+
+	private OnlineStore onlineStore;
+
+	@Before
+	public void setUp() {
+		 onlineStore = new OnlineStore();
 	}
-
+	@Test
+	public void testInstanceOf() {
+        assertEquals(true, onlineStore instanceof OnlineStore);
+	}
 }
