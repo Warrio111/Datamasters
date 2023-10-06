bba18dcb5c38c892c0295ecc7a58282b88e10a37
Robert Benavides <robertbenavidesgomez@gmail.com>
2023-10-07 01:11:32 +0200

    Double Circular Linked LIST

--- src/main/java/com/datamasters/modelo/List.java
+++ src/main/java/com/datamasters/modelo/List.java
@@ -1,11 +1,18 @@
 package com.datamasters.modelo;
-
+/*
+This is our personal implementation of double ciruclar linked list in Java
+Robert has studied this topic in the past and he has implemented this code from scratch
+You can find the Udemy mastering data structures and algorithms course from Abdul Bari:
+https://www.udemy.com/course/datastructurescncpp/
+ */
 public class List<T> {
     private Node<T> first;
+    private Node<T> last;
     private int size;
 
     public List() {
         first = null;
+        last = null;
         size = 0;
     }
 
@@ -15,36 +22,47 @@ public class List<T> {
 
     public void add(T t) {
         Node<T> newNode = new Node<>(t);
-        if (first == null) {
+        if (isEmpty()) {
             first = newNode;
+            last = newNode;
+            first.setNext(last);
+            last.setPrevious(first);
         } else {
-            Node<T> current = first;
-            while (current.getNext() != null) {
-                current = current.getNext();
-            }
-            current.setNext(newNode);
+            newNode.setPrevious(last);
+            last.setNext(newNode);
+            last = newNode;
+            last.setNext(first);
+            first.setPrevious(last);
         }
         size++;
     }
 
     public void delete(T t) {
-        if (first == null) {
+        if (isEmpty()) {
             return;
         }
-        if (first.getValue().equals(t)) {
-            first = first.getNext();
-            size--;
-            return;
-        }
-        Node<T> previous = first;
-        Node<T> current = first.getNext();
-        while (current != null && !current.getValue().equals(t)) {
-            previous = current;
+        Node<T> current = first;
+        while (current != null) {
+            if (current.getValue().equals(t)) {
+                if (size == 1) {
+                    first = null;
+                    last = null;
+                } else if (current == first) {
+                    first = current.getNext();
+                    first.setPrevious(last);
+                    last.setNext(first);
+                } else if (current == last) {
+                    last = current.getPrevious();
+                    last.setNext(first);
+                    first.setPrevious(last);
+                } else {
+                    current.getPrevious().setNext(current.getNext());
+                    current.getNext().setPrevious(current.getPrevious());
+                }
+                size--;
+                return;
+            }
             current = current.getNext();
-        }
-        if (current != null) {
-            previous.setNext(current.getNext());
-            size--;
         }
     }
 
@@ -61,6 +79,7 @@ public class List<T> {
 
     public void clear() {
         first = null;
+        last = null;
         size = 0;
     }
 
@@ -74,6 +93,9 @@ public class List<T> {
         while (current != null) {
             arrayList.add(current.getValue());
             current = current.getNext();
+            if(current == first) {
+                break;
+            }
         }
         return arrayList;
     }
@@ -81,10 +103,11 @@ public class List<T> {
     private static class Node<T> {
         private T value;
         private Node<T> next;
-
+        private Node<T> previous;
         public Node(T value) {
             this.value = value;
             this.next = null;
+            this.previous = null;
         }
 
         public T getValue() {
@@ -92,11 +115,18 @@ public class List<T> {
         }
 
         public Node<T> getNext() {
-            return next;
+            return this.next;
         }
-
         public void setNext(Node<T> next) {
             this.next = next;
         }
+        public  Node<T> getPrevious() {
+            return this.previous;
+        }
+        public  void setPrevious(Node<T> previous) {
+            this.previous = previous;
+        }
+
+
     }
 }
